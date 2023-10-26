0d882ff4caac2d8cf3d32d9247bc6c1b718f2cef
Donald Ajaps <ajapon@yahoo.com>
2023-10-26 08:48:47 +0100

    Fix 0x04: UTF-8 Validation
    + Adds code mitigate validate overlong encodings
    + Adds a function print_utf8 that prints a chart of all binary bits from 0 to 255.

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Log parsing challenge"""
+"""UTF-8 validation challenge"""
 from typing import List, Union, Optional
 
 
@@ -13,7 +13,9 @@ def validUTF8(data: List[int], verbose: Optional[bool] = False) -> bool:
             or False in [isinstance(x, int) for x in data]:
         return False
 
+    # Validate data byte-by-byte and return False if an invalid byte is found
     continuation_bytes = 0
+    previous_byte = 0
     for num in data:
         # Convert integer to byte/eight least significant bits (256 == 2^8)
         num %= 256
@@ -23,8 +25,15 @@ def validUTF8(data: List[int], verbose: Optional[bool] = False) -> bool:
             left = continuation_bytes
             print(f'{num}[{left}], ' if left else f'{num}, ', end='')
 
-        if num >= 254:
-            # Invalid byte -> 254 or 255 > maximum 253 '1111110x' for UTF-8
+        if num >= 248:
+            # Invalid byte -> 248 to 255 > maximum 247 '11110111' for UTF-8
+            return False
+        # if num in (192, 193, 224, 225):
+        if num in (192, 193):
+            # Leading bytes for overlong encodings -> '11000000' and '11000001'
+            return False
+        if num == 128 and previous_byte in (224, 240):
+            # Overlong encoding -> '11100000 10000000' or '11110000 10000000'
             return False
         if num < 128:
             # Continuation byte '10xxxxxx' expected but not found '0xxxxxxx'
@@ -37,21 +46,41 @@ def validUTF8(data: List[int], verbose: Optional[bool] = False) -> bool:
                 return False
             # Continuation byte found, update remainder
             continuation_bytes -= 1
-        elif num >= 192:
+        elif num >= 194:
             # Continuation byte '10xxxxxx' expected but not found '11xxxxxx'
             if continuation_bytes:
                 return False
-            # Character with multiple bytes found. Get length/number of bytes
+            # Leading byte found. Get length/number of continuation bytes
             continuation_bytes = len(bin(num)[2:].split('0')[0]) - 1
+        # Track current byte for overlong encodings checks
+        previous_byte = num
 
-        # Valid byte. Move to next byte in sequence
-
-    # No invalid bytes found in sequence after parsing all bytes
-    return True
+    # Return True if sequence ended as expected, else False
+    return True if not continuation_bytes else False
 
 
 if __name__ == '__main__':
     """Tests the code in this module"""
+    def print_utf8() -> None:
+        """Prints a chart of all binary bits from 0 to 255"""
+        for x in range(16):
+            if not x:
+                print(f'{" ":^4}', end='')
+            print(f'{hex(x)[-1].upper():^12} ', end='')
+        print()
+        i = 1
+        for x in range(256):
+            if not x % 16:
+                print(f' {hex(x // 16)[-1].upper():}x ', end='')
+            print(f'{bin(x)[2:]:0>8}|{x:<3} ', end='')
+            if not i % 16:
+                print()
+            i += 1
+        print()
+
+    # print_utf8()
+    # exit()
+
     def print_decimal_binary(dec: Union[int, list]) -> None:
         """Convert one/list of decimal(s) to binary then prints it/them"""
         if not isinstance(dec, (int, list)):
@@ -59,7 +88,7 @@ if __name__ == '__main__':
         dec = [dec] if isinstance(dec, int) else dec
         for num in dec:
             num %= 256
-            print(f'{num:>8}: {bin(num)[2:]:0>8} <- {bin(num)}')
+            print(f'{num:>8}: {bin(num)[2:]:0>8} <- {bin(num):010}')
 
     data = [65]
     print(validUTF8(data, verbose=True))
@@ -73,4 +102,18 @@ if __name__ == '__main__':
     data = [229, 65, 127, 256]
     print(validUTF8(data, verbose=True))
     print('ERROR: Continuation byte "10xxxxxx" expected but not found @65')
+    print_decimal_binary(data)
+
+    data = [245, 247]
+    print(validUTF8(data, verbose=True))
+    print('ERROR: Continuation byte "10xxxxxx" expected but not found @247')
+    print_decimal_binary(data)
+
+    data = [245, 147, 187, 142, 125, 0]
+    print(validUTF8(data, verbose=True))
+    print_decimal_binary(data)
+
+    data = [245, 147]
+    print(validUTF8(data, verbose=True))
+    print('ERROR: Continuation byte "10xxxxxx" expected, not found after 147')
     print_decimal_binary(data)
