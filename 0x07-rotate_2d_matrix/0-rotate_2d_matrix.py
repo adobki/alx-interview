aa3c09c4d6191bdb9afc0a59d2ab1be1685852f9
Donald Ajaps <ajapon@yahoo.com>
2023-11-14 07:45:41 +0100

    Fix tasks in 0x07: Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -15,42 +15,29 @@ def print_matrix(matrix: list) -> None:
             print(f' {row}')
 
 
-def rotate_idx(row: int = 0, col: int = 0, __factor: int = 1, __limit: int = 2,
-               size: int = 2) -> tuple:
-    """Get next index of 2D matrix in clockwise direction"""
-    col += __factor
-    if col == __limit + __factor:
-        col, row = __limit, row + __factor
-    if row == __limit + __factor:
-        __factor *= -1
-        col, row = col + __factor, __limit
-        __limit = 0 if __factor < 0 else size
-    return row, col, __factor, __limit
-
-
 def rotate_2d_matrix(matrix: list) -> None:
     """Rotates a 2D matrix 90 degrees clockwise"""
     # Input Validation: Check that matrix is a matrix (a list of lists)
     if not matrix or not isinstance(matrix, list):
         return
+    # Input Validation: Check for square matrix (equal number of rows/columns)
     for row in matrix:
-        if not row or not isinstance(row, list):
+        if not row or not isinstance(row, list) or not len(row) == len(matrix):
             return
 
-    # Define parameter defaults for rotate_idx function
-    row, col, operand, limit = 0, 0, 1, len(matrix) - 1
+    # Create empty matrix with same size as given matrix
+    size = len(matrix)
+    rotated = [[0 for _ in range(size)] for _ in range(size)]
+    # Copy contents of original to new matrix with values rotated 90 degrees
+    for row in range(size):
+        c = size - 1 - row
+        for col in range(size):
+            rotated[col][c] = matrix[row][col]
 
-    # Rotate matrix values 90 degrees in clockwise direction
-    for _ in range(len(matrix) - 1):
-        # Shift matrix values backward one cell in counter-clockwise direction
-        for _ in range(len(matrix) * len(matrix) - 2):
-            # Set current index before shifting
-            idx = col, row
-            # Get row and column indexes of next cell
-            row, col, operand, limit = rotate_idx(row, col, operand, limit)
-            # Replace value in cell with previous value and backup current
-            matrix[col][row], matrix[idx[0]][idx[1]] = \
-                matrix[idx[0]][idx[1]], matrix[col][row]
+    # Copy values from rotated matrix back to original
+    for row in range(size):
+        for col in range(size):
+            matrix[row][col] = rotated[row][col]
 
 
 if __name__ == '__main__':
@@ -63,9 +50,32 @@ if __name__ == '__main__':
     rotate_2d_matrix(matrix)
     print_matrix(matrix)
 
-    matrix = [['one  ', 'two  ', 'three'],
-              ['four ', 'five ', 'six  '],
-              ['seven', 'eight', 'nine ']]
+    matrix = [[1, 2],
+              [3, 4]]
+
+    print_matrix(matrix)
+    rotate_2d_matrix(matrix)
+    print_matrix(matrix)
+
+    matrix = [[1, 2, 3, 4],
+              [5, 6, 7, 8],
+              [9, 10, 11, 12],
+              [13, 14, 15, 16]]
+
+    print_matrix(matrix)
+    rotate_2d_matrix(matrix)
+    print_matrix(matrix)
+
+    matrix = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+              [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+              [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+              [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+              [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+              [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+              [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+              [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+              [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+              [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]]
 
     print_matrix(matrix)
     rotate_2d_matrix(matrix)
